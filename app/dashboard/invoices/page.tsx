3899af04ff0ea175722e937f21ac458e4c7e8a4d
Isana Koto <isn.kumano@gmail.com>
2023-11-03 00:30:12 +0900

    init

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="animate-fade-in">
+      <p>Invoices Page</p>
+    </div>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import {Inter, Lusitana, Rampart_One} from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin']})
+export const lusitana = Lusitana({ weight:['400','700'],subsets: ['latin']})
+export const rampartone = Rampart_One({
+  weight: '400',subsets: ['latin']
+})
